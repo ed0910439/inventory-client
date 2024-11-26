016bce0a8cab6542970dfbfcdf9dd186651192f1
Edward <79468365+ed0910439@users.noreply.github.com>
2024-11-26 16:39:01 +0800

    Update generate-version.js

--- generate-version.js
+++ generate-version.js
@@ -3,23 +3,17 @@ const path = require('path');
 
 const dir = path.join(__dirname, 'build');
 if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, { recursive: true }); // 確保目錄存在
+    fs.mkdirSync(dir, { recursive: true }); // ç¢ºä¿ç›®éŒ„å­˜åœ¨
 }
-// Åª¨ú package.json
+// Ã…ÂªÂ¨Ãº package.json
 const packageJson = require('./package.json');
 
-// ´£¨úª©¥»¸¹
+// Â´Â£Â¨ÃºÂªÂ©Â¥Â»Â¸Â¹
 const version = {
     version: packageJson.version
 };
 
-<<<<<<< HEAD
-// �N�������g�J version.json
-const filePath = path.join(__dirname, public, 'version.json');
-=======
-// ±Nª©¥»¸¹¼g¤J version.json
 const filePath = path.join(__dirname, 'build', 'version.json');
->>>>>>> 051dc4e7f9409213b1167ca8111bfb56b0a941ba
 fs.writeFileSync(filePath, JSON.stringify(version, null, 2));
 
 console.log('Generated version.json successfully.');
